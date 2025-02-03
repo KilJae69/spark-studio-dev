b0eb5c06d2e6f6c71f13cc75d62f676c952e2f45
KilJae69 <adi.toromanovic@outlook.com>
2025-02-03 10:42:40 +0100

    .

--- components/sections/CaseStudiesSection.tsx
+++ components/sections/CaseStudiesSection.tsx
@@ -71,32 +71,28 @@ export default function CaseStudiesSection({
         </FadeInStagger>
       </Container>
       <Container className="mt-16">
-        <FadeInStagger className="grid grid-cols-1 gap-20 lg:grid-cols-3">
+        <FadeInStagger className="grid grid-cols-1 gap-5 lg:grid-cols-3">
           {caseStudies.map((caseStudy) => (
-            <FadeIn key={caseStudy.href} className=" ">
+            <FadeIn
+              key={caseStudy.href}
+              className=" h-[25rem] w-full flex items-center justify-center"
+            >
               {isInView && (
                 <PinContainer
                   className=" group"
                   href={caseStudy.href}
                   title={caseStudy.title}
                 >
-                  <div className="flex basis-full flex-col p-1  sm:p-4 w-[16rem] sm:w-[27rem] tracking-tight text-primary-100/50 lg:w-[20rem] h-[20rem] ">
-                    <h3 className="max-w-xs min-h-16 !pb-2 !m-0 font-bold  text-base text-white group-hover:text-primary-accent transition">
-                      {caseStudy.title}
+                  <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                    <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                      Aceternity UI
                     </h3>
                     <div className="text-base !m-0 !p-0 font-normal">
-                      <span className="text-slate-500 line-clamp-3">
-                        {caseStudy.description}
+                      <span className="text-slate-500 ">
+                        Customizable Tailwind CSS and Framer Motion Components.
                       </span>
                     </div>
-                    <div className="mt-2 relative overflow-hidden w-full h-full rounded-xl">
-                      <Image
-                        className="grayscale group-hover:grayscale-0 transition-all object-cover"
-                        fill
-                        src={caseStudy.image.src}
-                        alt={caseStudy.title}
-                      />
-                    </div>
+                    <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
                   </div>
                 </PinContainer>
               )}
