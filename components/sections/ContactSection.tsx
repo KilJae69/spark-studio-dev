1dc88a516d1845b114fd6a23e8f94d2a5156b8d7
KilJae69 <adi.toromanovic@outlook.com>
2025-02-18 11:52:00 +0100

    .

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -2,25 +2,26 @@
 import { Button } from "@/components/Button";
 import { Container } from "@/components/Container";
 import { FadeIn } from "@/components/FadeIn";
-import useLazyLoad from "@/hooks/useLazyLoad";
+// import useLazyLoad from "@/hooks/useLazyLoad";
 import { useTranslations } from "next-intl";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import Image from "next/image";
+import LottieComponent from "../shared/LottieComponent";
 
 
-const DynamicLottieComponent = dynamic(() => import("@/components/shared/LottieComponent"), {
-  ssr: false,
-})
+// const DynamicLottieComponent = dynamic(() => import("@/components/shared/LottieComponent"), {
+//   ssr: false,
+// })
 
 export function ContactSection() {
   const t = useTranslations("ContactSection");
-  const {ref,isInView} = useLazyLoad()
+
   
 
   return (
     <Container className="mt-20">
       <FadeIn className="relative rounded-md  overflow-hidden -mx-6 sm:rounded-4xl bg-gradient-to-tr bg-primary-800 px-6 py-20 sm:mx-0  md:px-12">
-        <div ref={ref} className="mx-auto max-w-4xl pb-20 flex flex-col sm:flex-row justify-between ">
+        <div  className="mx-auto max-w-4xl pb-20 flex flex-col sm:flex-row justify-between ">
           <div className="max-w-xl flex-1">
             <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
               {t("title")}
@@ -51,7 +52,7 @@ export function ContactSection() {
           </div>
         </div>
         <div className=" absolute bottom-0 translate-y-[50%] lg:translate-x-1/2 lg:right-20 lg:bottom-20 right-0 left-0">
-          {isInView && <DynamicLottieComponent path="/animations/globe.json"/>}
+       <LottieComponent path="/animations/globe.json"/>
         </div>
       </FadeIn>
     </Container>
