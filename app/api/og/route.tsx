16f157c334a3a50ef981f8c82e040bf2c20c0f5e
KilJae69 <adi.toromanovic@outlook.com>
2025-02-19 17:36:26 +0100

    .

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -17,8 +17,8 @@ export async function GET(request: Request) {
       ? searchParams.get("description")?.slice(0, 140)
       : "Your go-to platform for creativity and innovation.";
 
-    const hasOGCTA1 = searchParams.has("ogCTA1")
-    const hasOGCTA2 = searchParams.has("ogCTA2")
+    const hasOGCTA1 = searchParams.has("ogCTA1");
+    const hasOGCTA2 = searchParams.has("ogCTA2");
 
     const ogCTA1 = hasOGCTA1
       ? searchParams.get("ogCTA1")?.slice(0, 100)
@@ -34,18 +34,24 @@ export async function GET(request: Request) {
     const logoData = await fetch(
       new URL("../../../public/spark-logo-img.png", import.meta.url)
     ).then((res) => res.arrayBuffer());
+    const avatarData = await fetch(
+      new URL("../../../public/og-avatar-resized.png", import.meta.url)
+    ).then((res) => res.arrayBuffer());
 
     // Convert ArrayBuffer to base64
     const logoBase64 = Buffer.from(logoData).toString("base64");
     const logoUrl = `data:image/png;base64,${logoBase64}`;
 
+    const avatarBase64 = Buffer.from(avatarData).toString("base64");
+    const avatarUrl = `data:image/png;base64,${avatarBase64}`;
+
     return new ImageResponse(
       (
         <div tw="flex flex relative w-full h-full items-center justify-center bg-white ">
-          <div tw="flex flex-col w-full py-12 px-4 md:items-center justify-between p-8">
+          <div tw="flex flex-col w-[70%] py-12 px-4 md:items-center justify-between p-8">
             {/* Image with responsive styling */}
 
-            <img src={logoUrl} alt="spark logo" width={300} height={120} />
+            {/* <img src={logoUrl} alt="spark logo" width={300} height={120} /> */}
             <h2
               style={{ fontFamily: "Poppins" }}
               tw="flex flex-col text-3xl sm:text-4xl font-bold py-8 tracking-wide text-center text-[#d4af37] justify-center items-center"
@@ -65,6 +71,10 @@ export async function GET(request: Request) {
                 </a>
               </div>
             </div>
+          </div>
+          <div tw="w-[30%] flex flex-col pt-8 items-center justify-between h-full bg-slate-800">
+          <img src={logoUrl} alt="spark logo" tw="ml-8" width={300} height={120} />
+            <img src={avatarUrl} alt="avatar" width={300} height={400} />
           </div>
         </div>
       ),
